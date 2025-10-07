fac34321b0121e13269175041326edbb7e67a834
Rohit Rankmantra <rohitrankmantra12@gmail.com>
2025-10-07 13:11:02 +0530

    integrate backend

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000/api', // ✅ backend URL
+  baseURL: "https://aayu-backend-9ghq.onrender.com/api" || 'http://localhost:4000/api', // ✅ backend URL
   withCredentials: true, // ✅ allows sending & receiving cookies
 
 });
